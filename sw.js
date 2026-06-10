1e24cef9bfdad738ffc2ab03039337adcdcb7cbf
DA77ashyt <da77ashyt@users.noreply.github.com>
2026-06-10 17:08:22 +0800

    Initial commit: fridge manager PWA

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'fridge-manager-v1';
+const urlsToCache = [
+    './',
+    './index.html'
+];
+
+// Install
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+// Fetch
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+// Activate
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.filter(name => name !== CACHE_NAME)
+                    .map(name => caches.delete(name))
+            );
+        })
+    );
+});
